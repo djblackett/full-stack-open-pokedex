dd76910b4c0634f564f546dce7e73dd87531f6c9
djblackett <49618491+djblackett@users.noreply.github.com>
2023-09-02 19:38:21 -0300

    add cypress tests

---
+++ cypress/e2e/pokemon.cy.js
@@ -0,0 +1,18 @@
+describe('Pokedex', function () {
+    it('front page can be opened', function () {
+        cy.visit('http://localhost:5000')
+        cy.contains('ivysaur')
+        cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    })
+
+    it('can navigate to next pokemon', function () {
+        cy.visit("http://localhost:5000/");
+        cy.contains('caterpie').click()
+        cy.contains("Next").click()
+        cy.contains('metapod')
+        cy.contains("shed skin")
+
+    })
+
+})
+
